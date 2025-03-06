a69ca1ccb84fec624e5d703bebd4e04e788f6c38
NathanVnh <150239886+NathanVnh@users.noreply.github.com>
2025-03-06 22:20:55 +0100

    Update local_paths.py

--- src/local_paths.py
+++ src/local_paths.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 # put the absolute path of the repository
 # run the command `pwd` if you are on a UNIX-like system
-REPO_PATH = Path("")
+REPO_PATH = Path("/home/onyxia/work/mgdm")
